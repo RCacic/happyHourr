3ccd7e52c3ec2ee818198011c53d9373e1ac1f1f
RCacic <104887197@student.swin.edu.au>
2026-05-20 15:49:16 +1000

    Create frontend/sampleData.js

---
+++ frontend/sampleData.js
@@ -0,0 +1,18 @@
+const venues = [
+  {
+    name: "Zsa's",
+    suburb: "Thornbury",
+    happyHour: "5pm - 7pm",
+    deal: "$5-$10 plates",
+    transport: "86 Tram"
+  },
+  {
+    name: "Peacock Inn",
+    suburb: "Northcote",
+    happyHour: "7pm onwards",
+    deal: "Late night drinks",
+    transport: "86 Tram"
+  }
+];
+
+export default venues;
